8ce957e707b1025f29d32295c61e5e4218f5a6d8
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-02-27 10:14:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ image = Image.open('bingo.jpg')
 st.image(image, caption= 'te quiero mucho bingo')
 
 
-texto = st.text_input(´Esta es mi primera app´ , 'la estoy haciendo de bingo')
+texto = st.text_input('Esta es mi primera app' , 'la estoy haciendo de bingo')
 st.write('El texto escrito es', texto)
