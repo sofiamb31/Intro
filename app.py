19c2cc3fc24a5b60e9d2188ffdeff42cca058152
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-02-27 10:10:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title(" Mi Primera App Sofi")
 
 st.header("Bingo")
 st.write("Yo soy Bingo, Bingo soy yo")
-image = Image.open('bingo.jpg)
+image = Image.open('bingo.jpg')
 
 st.image(image, caption= 'Interfaces multimodales')
