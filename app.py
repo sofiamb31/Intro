daa4133e7ce5b612b91fe8c48de97e0cb0d8aed0
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-02-27 10:25:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,14 @@ st.image(image, caption= 'te quiero mucho bingo')
 
 texto = st.text_input('Esta es mi primera app' , 'la estoy haciendo de bingo')
 st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Bingo mejora la experiencia del usuario viendo Bluey")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Correcto!')
