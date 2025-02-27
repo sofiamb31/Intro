9ef620efe1fc9be3a3ee0aab917fcafebedd389a
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-02-27 10:13:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ st.header("Bingo")
 st.write("Yo soy Bingo, Bingo soy yo")
 image = Image.open('bingo.jpg')
 
-st.image(image, caption= 'Interfaces multimodales')
+st.image(image, caption= 'te quiero mucho bingo')
+
+
+texto = st.text_input(´Esta es mi primera app´ , 'la estoy haciendo de bingo')
+st.write('El texto escrito es', texto)
