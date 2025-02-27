88bfa6cce470d6fc91d6148661f807a9eb6e6eee
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-02-27 10:10:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
-
+from PIL import image
 
 st.title(" Mi Primera App Sofi")
+
+st.header("Bingo")
+st.write("Yo soy Bingo, Bingo soy yo")
+image = Image.open('bingo.jpg)
+
+st.image(image, caption= 'Interfaces multimodales')
